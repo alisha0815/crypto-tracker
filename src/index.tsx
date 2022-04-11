f06dfedbced8b7337f2fec192661b1d43760c8d7
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-04-11 18:19:09 +0200

    theme provider setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { theme } from "./theme";
+// import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// const root = createRoot(rootElement!);
