d7116f771c6370c44aff1171aaf7355e3b2a70b8
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-04-12 15:27:43 +0200

    fixed bug related to react 8 version

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { theme } from "./theme";
-// import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // const root = createRoot(rootElement!);
