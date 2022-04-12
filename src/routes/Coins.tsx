ec9a32ff4e864c5fc3caf25f73e4450873e582f2
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-04-12 16:36:27 +0200

    global style setup

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
 const Coins = () => {
+  const Title = styled.div`
+    color: ${(props) => props.theme.accentColor};
+  `;
   return (
-    <div>
+    <Title>
       <h2>Coins</h2>
-    </div>
+    </Title>
   );
 };
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#40407a",
+  textColor: "#f7f1e3",
+  accentColor: "#ffda79",
 };
