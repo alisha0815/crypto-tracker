0cd9b1faf8a20d08e1dd73eb94f255bf08505ff9
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-04-13 11:25:27 +0200

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { theme } from "./theme";
+import { theme } from "./styles/theme";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 
