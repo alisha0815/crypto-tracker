3a073fbee65b7c5edb4f765623cc4877d9e78ae5
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-05-07 16:41:32 +0200

    fetching coin data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { ICoin } from "../Interface/CoinInterface";
 
-interface ICoin {
-  name?: string;
-  id?: string;
-}
-const Coin: React.FC<ICoin> = ({ name, id }: ICoin) => {
+// interface ICoin {
+//   name?: string;
+//   id?: string;
+// }
+const Coin = () => {
   const { coinId } = useParams();
+  const baseUrl = `https://api.coinpaprika.com/v1/coins/${coinId}`;
+
+  console.log("coinId", coinId);
+  console.log("url", baseUrl);
+
+  const fetchCoin = async () => {
+    const res = await (await fetch(baseUrl)).json();
+    // const json = await response.json();
+    console.log("json", res);
+  };
+
+  console.log("fetch coin", fetchCoin());
+
+  // useEffect(() => {
+  //   const coin = await fetch(baseUrl);
+  // }, []);
 
   return (
     <div>
