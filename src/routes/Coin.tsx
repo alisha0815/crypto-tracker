61475c961f0de474ab629e4a3fb96da0cc382d00
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-05-11 08:38:03 +0200

    coin info render

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ICoin } from "../Interface/CoinInterface";
+import { ICoin, IPrice } from "../Interface/CoinInterface";
 
 const Coin = () => {
   const { coinId } = useParams();
-  const [coinInfo, setCoinInfo] = useState({});
+  const [coinInfo, setCoinInfo] = useState<ICoin>();
+  const [price, setPtrice] = useState<IPrice>();
   const baseUrl = `https://api.coinpaprika.com/v1/coins/${coinId}`;
   const priceUrl = `https://api.coinpaprika.com/v1/tickers/${coinId}`;
   //api.coinpaprika.com/v1/tickers/btc-bitcoin
@@ -25,8 +26,7 @@ const Coin = () => {
       const coin = await fetchCoin();
       setCoinInfo(coin);
       const price = await fetchPrice();
-      console.log("price info", price);
-      console.log("coin info", coin);
+      setPtrice(price);
     })();
   }, []);
 
@@ -34,7 +34,12 @@ const Coin = () => {
     <div>
       <h3>Coin: {coinId}</h3>
       <ul>
-        <li>{coinInfo.name}</li>
+        <li>{coinInfo?.name}</li>
+        <li>{coinInfo?.rank}</li>
+        <li>{coinInfo?.symbol}</li>
+        <li>{coinInfo?.description}</li>
+        <li>{price?.quotes.USD.ath_price.toFixed(3)}</li>
+        <li>{price?.total_supply}</li>
       </ul>
     </div>
   );
